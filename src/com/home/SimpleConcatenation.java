ddeeb3998d1921d7c96a04103384f12b6336c674
Sivaa979 <kunchalasivaprasad143@gmail.com>
2024-05-26 21:29:02 +0530

    arrays programs

---
+++ src/com/home/SimpleConcatenation.java
@@ -0,0 +1,13 @@
+package com.home;
+
+public class SimpleConcatenation {
+    public static void main(String args[]){
+        String str1="Hii";
+        String str2="World";
+        System.out.print(str1. concat (str2));
+
+
+
+
+    }
+}
