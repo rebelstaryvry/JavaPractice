07ff804b62034ca4c85048ab6fca6d1f25650be2
Sivaa979 <kunchalasivaprasad143@gmail.com>
2024-05-27 12:27:57 +0530

    my bikes

---
+++ src/com/home/MyBikes.java
@@ -0,0 +1,7 @@
+package com.home;
+
+public class MyBikes {
+    public static void main(String args[]){
+        System.out.print("print Hero Honda");
+    }
+}
---
+++ src/com/home/SimpleFor.java
@@ -0,0 +1,10 @@
+package com.home;
+
+public class SimpleFor {
+    public static void main(String args[]){
+        int z = 10;
+        for(int i =1 ; i <= 10; i++){
+            System.out.print(z+"*" + i +"="+(z*i));
+        }
+    }
+}
