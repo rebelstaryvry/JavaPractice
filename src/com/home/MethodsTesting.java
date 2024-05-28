3b3c26314a5a0f460e9bc66bfcb0433e21b7dff9
Sivaa979 <kunchalasivaprasad143@gmail.com>
2024-05-28 19:56:54 +0530

    print method

--- src/com/home/MethodsTesting.java
+++ src/com/home/MethodsTesting.java
@@ -1,4 +1,12 @@
 package com.home;
 
 public class MethodsTesting {
+    static void abcd() {System.out.print("Number method");}
+    static void efgh() {System.out.print("Number method 2");}
+    static void xvyz() {System.out.print("Number method 3");}
+
+    public static void main(String args[]){
+        efgh();
+        System.out.println("number method 2");
+    }
 }
